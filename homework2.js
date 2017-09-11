39ef1b95f640d8fab02d5e8452dbb2159cc27036
Miqayel Miqayel <miqayelmiqayel@Miqayels-MacBook-Air.local>
2017-09-11 14:20:17 +0400

    First commit

---
+++ homework2.js
@@ -0,0 +1,11 @@
+hello = "Hello World";
+
+console.log(hello);
+
+const x = "5000";
+console.log (x + 555)
+
+console.log((4 + 3) * 10);
+  console.log(16/4);
+    console.log(10+20-30);
+    console.log(isNaN('true+ true'));
